825dce45e59e49c85a3241a8bc7e8c8a21222ed0
noiciboy <cristianavila@unisangil.edu.co>
2022-11-09 18:47:22 -0500

    se definen las variables que se van a usar

--- LABORATORIO.ino
+++ LABORATORIO.ino
@@ -8,3 +8,18 @@
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
+
+// defino variables 
+
+long duration;
+int D_ant,Distance,bandera,mnum,BANDERA;
+int LED = 13; 
+int LED1 = 12;
+int VENT = 14;  
+int LED_MAIL = 18;     //para prender el led indicador que se envio el mensaje
+int BOTON = 0;     //
+int mnuman = 0;
+int SENSOR = 4; // entrada del boton 
+
+String stringdistance,str,pta,hum,tmp,tem,mens,PUERTA,EMAIL;
+float  t,h,h_ant,t_ant;
